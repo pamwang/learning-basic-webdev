c72a57430712a6f3c823c6cc2ca8268858e9d905
Pamela Wang <pampam133@gmail.com>
2020-10-09 00:20:34 -0400

    js finished

--- script.js
+++ script.js
@@ -176,6 +176,7 @@ orderChickenWith();
 //shortcut: sideDish = sideDish || alternate;
 
 //-----------------------------------------------------
+/*
 //Creating Objects using "New Object()" Syntax
 var company = new Object();
 company.name = "comapanyName";
@@ -185,4 +186,369 @@ company.ceo.firstName = "Tim";
 company.lastname.iscool = "nah";
 //^ this doesnt work because lastname is undefined
 console.log("Company CEO is: " + company.ceo.firstName);
-console.log(company["name"])
+console.log(company["name"]); //this is the same as company.name
+//- dot operator only works with valid js names 
+
+//creating objects using object literal syntax
+//original method
+var a = new Object();
+//better way: object literal (non initialized)
+var b = {};
+//better way: object literal initialized
+var c = {
+    name: "Pamela",
+    ceo: {
+        firstName: "Pamela",
+        LastName: "wang"
+    },
+    $stock: 110,
+    "these can be strings": "oh yea"
+};
+
+console.log(c);
+
+*/
+
+//-------------------------------------------------------
+//Functions explained
+function multiply(x, y) {
+    return x * y;
+}
+//functions are first class datatypes - whatever you can do with a variable you can also do with functions
+//functions are objects with special properties
+
+console.log(multiply(5, 3));
+//expected: 15;
+
+//can set a property on the multiply function!
+multiply.version = "version 1.0.0";
+
+console.log(multiply);
+//expected: function multiply(x, y) {
+//    return x * y;
+//}
+
+console.log(multiply.version);
+//expected: version 1.0.0
+
+//FUNCTION FACTORIES
+function makeMultiplier(multiply) {
+    var myFunc = function(x) {
+        return multiplier * x;
+    };
+
+    return myFunc;
+    //value of myFunc will return
+}
+
+var multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(10));
+//expected = 30
+
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(100));
+//expected: 200;
+
+//passing functions as arguments
+function doOperationOn(x, operation) {
+    return operation(x);
+}
+
+var result = doOperationOn(5, multiplyBy3);
+console.log(result);
+//expected: 15
+
+result = doOperation(100, doubleAll);
+console.log(result);
+//expected 200
+
+//can treat functions as objects
+
+//------------------------------------------------------------
+//passing variables by value VS by reference
+
+//Passing by Value: given b=a, changing b will not change a & vice versa
+//Passing by reference: given b=a; changing b DOES change a & vice versa
+
+//Primitaives: copied by value
+//Objects: passed by reference
+
+//Primatives: var = 7;
+//var b=a;
+// b = 7
+
+//example: by reference VS by value
+
+//by value
+var a = 7;
+var b = a;
+console.log("a: " + a);
+console.log("b: " + b);
+//expected a: 7 b:7
+
+b = 5;
+console.log("a: " + a);
+console.log("b: " + b);
+//expected: a:7 b:5
+
+//by reference
+var c = { x: 7 };
+var d = c;
+console.log("c: " + c);
+console.log("d: " + d);
+//expected: Object{x:7} Object{x:7}
+
+d.x = 5;
+console.log("c: " + c);
+console.log("d: " + d);
+//expected: Object{x:5} Object{x:5}
+
+//passing by value in functions
+
+function changePrimitive(primVal) {
+    console.log("Before: " + primValue);
+    primVal = 5;
+    console.log("after: ");
+    console.log(primVal);
+    //expected 5;
+}
+
+var value = 7;
+changePrimitive(value);
+console.log("original value:" + value);
+//expected: 7
+
+//passing by reference in functions
+function changeObject(objVal) {
+    console.log("Before: " + objValue);
+    objVal.x = 5;
+    console.log("after: ");
+    console.log(objVal);
+    //expected 5;
+}
+
+var value = 7;
+changeObj(value);
+console.log("original value:" + value);
+//expected: 7
+
+//------------------------------------------------------
+//Functions Constructors, prototypes, and this keyword
+//way to create objects in js
+
+function test() {
+    console.log("Hello");
+    console.log(this);
+    //expected: the global window object
+
+    this.myName = "pam";
+
+    //declaring a function as a parameter into test object
+    this.getArea = function() {
+        return Math.PI * Math.pow(this.radius, 2);
+    };
+}
+
+test();
+console.log(window.myName);
+//expected: "pam"
+
+//constructors
+//capital for function constructor
+function Circle(radius) {
+    console.log(this);
+    this.radius = radius;
+
+    //declaring a function as a parameter into test object
+    this.getArea = function() {
+        return Math.PI * Math.pow(this.radius, 2);
+    };
+}
+
+//FUNCTION NEEDED ******************* **********************
+var myCircle = new Circle(10);
+//makes this point to the circle
+
+console.log(myCircle);
+//expected: empty object with the name, "type" = circle
+
+console.log(myCircle.getArea);
+//area
+
+//creating prototypes
+//should not be in the Circle constructor because you don't need a
+//new prototype each time
+Circle.prototype.getArea = function() {
+        return Math.PI * Math.pow(this.radius, 2);
+    } //expected output: only the radisu NOT getArea
+    //getArea is located under proto function (special)
+
+
+//-----------------------------------------------------------
+//Object literals and "this"
+
+//object literal
+var literalCircle = {
+    radius: 10;
+    getArea: function() {
+        var self = this; //BUG FIX
+        console.log(this);
+        //expected output radisu:10
+        //this refers to circle class NOT the global window
+        //why does this work? Because object literals only refer
+        //to local scope
+
+
+        //NOTE: BUG
+        var increaseRadius = function() {
+            this.radius = 20;
+            //BUG: radius does not change to 20
+            //when you have a nested function, this starts pointing to the global object
+            //to get around this: var self = this;
+        };
+
+        increaseRadius();
+
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+};
+
+literalCircle.getArea();
+//expected output: the area
+
+//----------------------------------------------------------------
+//Arrays in JS
+//create array *****************************************
+var array = new Array();
+array[0] = "Pamela";
+array[1] = 2;
+array[2] = function(name) {
+    console.log("Hello" + name);
+};
+array[3] = { course: "JS" };
+console.log(array);
+// expected: ["Pamela", 2, function(name), Object length:4]
+
+console.log(array[1]);
+//expected: 2
+
+array[2]("My Name");
+//this will invoke the function located at position 2
+//expected: Hello My Name
+//this is the same as:
+array[2](array[0]);
+
+//shorthand array notation**********************************
+var names = ["pam", "wang", "test", ];
+
+console.log(names);
+//expected output: ["pam", "wang", "test"]
+
+for (var i = 0; i < names.length; i++) {
+    console.log("Hello" + names[i]);
+}
+//output all the arrays
+
+//arrays can be SPARSE
+names[100] = "Jim";
+//expected output: 
+//Hello pam
+//Hello wang
+//Hello test
+//Hello undefined
+//Hello Jim
+
+var myObj = {
+    name: "pam",
+    course: "HTML/CSS/JS",
+    platform: "Coursera"
+}
+
+//will traverse the object
+for (var prop in myObj) {
+    console.log(prop _ + ": " + myObj[prop]);
+}
+//expected output:
+/*  name: "pam",
+    course: "HTML/CSS/JS",
+    platform: "Coursera"
+*/
+
+//issue: will output ALL properties, even over unapplicable data
+for (var names in name) {
+    console.log("Hello " + name[names]);
+}
+
+//expected output: 
+/*  name: "pam",
+    course: "HTML/CSS/JS",
+    platform: "Coursera"
+*/
+
+//-----------------------------------------------------------
+//Javascript closures
+//SUPERDUPER IMPORTANT********************************************
+
+//example: 
+function makeMultiplier(multiplier) {
+
+    function b() {
+        console.log("Multiplier is: " + multiplier);
+    }
+    b();
+    //expected: 2
+
+    //doesn't create execution environment 
+    //does not get outer reference
+    //does not get this variable
+    return (
+        function(x) {
+            return multiplier * x;
+        }
+
+        //since this returns within a return
+        //js preserves the outer environment
+    )
+}
+
+var doubleAll = makeMultipler(2);
+console.log(doubleAll(10)); //return function only gets its own exec env
+//expected output: 20
+
+//---------------------------------------------------------------------------
+//Fake namespaces
+//occurs when functions step on the toes of other functions
+//refer to lecture 52 part 1
+//IMMEDIATELY INVOKED FUNCTION EXPRESSIONS IIFEs
+//don't use fake namespaces
+
+//example of IIFE
+(function() {
+    console.log("Something");
+})();
+//this produces and invokes a function at the same time
+//use this in the script function so that it executes the second it loads
+//pass the window object as a param of the function
+/*
+(function(window){
+    //TODO CODE HERE
+
+    window.fakeNamespaceName = fakeNamespaceName;
+})(window);
+*/
+
+//DOM MANIPULATION----------------------------------
+//window.document = entire html page
+//document.getelementbyId("title");
+//include script tag at the very bottom of the html file
+//HTMLDocument : gives all functions that can be used to manipulate webpage
+
+//DOM MANIPULATION------------------------------------
+//onclick = "sayHello();"
+function sayHello() {
+    var name = document.getElementById("name").value;
+    var message = "Hello " + name;
+
+    document.getElementById("content").textContent = message;
+}
